40caa385145d71909e201c415a267a87178a27b9
Sarah Maris <sarah@sarahmaris.com>
2017-03-07 09:03:58 -0500

    Add initial index.html and images

---
+++ js/data.js
@@ -0,0 +1,49 @@
+var cards = [{
+    name: "Pikachu",
+    image: "pikachu.png"
+  },
+  {
+    name: "Bulbasaur",
+    image: "bulbasaur.png"
+  },
+  {
+    name: "Charmander",
+    image: "charmander.png"
+  },
+  {
+    name: "Master Ball",
+    image: "masterball.png"
+  },
+  {
+    name: "Great Ball",
+    image: "greatball.png"
+  },
+  {
+    name: "Poke Ball",
+    image: "pokeball.png"
+  },
+  {
+    name: "Premier Ball",
+    image: "premierball.png"
+  },
+  {
+    name: "Meowth",
+    image: "meowth.png"
+  },
+  {
+    name: "Snorlax",
+    image: "snorlax.png"
+  },
+  {
+    name: "Wigglypuff",
+    image: "wigglypuff.png"
+  },
+  {
+    name: "Squirtle",
+    image: "squirtle.png"
+  },
+  {
+    name: "Egg",
+    image: "egg.png"
+  }
+];
